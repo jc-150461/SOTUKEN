cdadbf23001ee4d91430f6e62859e9c6d345be8e
燎 石橋 <150461@jc-21.jp>
2017-11-30 11:28:04 +0900

    初回

--- SOTUKEN/SOTUKEN/App.xaml.cs
+++ SOTUKEN/SOTUKEN/App.xaml.cs
@@ -12,8 +12,6 @@ namespace SOTUKEN
         public App()
         {
             InitializeComponent();
-
-            MainPage = new SOTUKEN.MainPage();
         }
 
         protected override void OnStart()
